c577dfa2cd539e056d3302141e1040e1ac2fd6f4
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-25 23:22:39 +0200

    pretty print support

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,9 @@ import qualified "glualint-lib" GLuaFixer.LintSettings as Settings
 import qualified "glualint-lib" GLuaFixer.AG.ASTLint as Lint
 import           "glualint-lib" GLuaFixer.LintMessage (LintMessage (..), sortLintMessages)
 import qualified "glualint-lib" GLuaFixer.Util as Util
+import qualified "glualint-lib" GLua.Parser as P
+import qualified "glualint-lib" GLua.AG.PrettyPrint as PP
+import qualified "glualint-lib" GLuaFixer.AG.DarkRPRewrite as DarkRP
 import           "reflex-dom"   Reflex.Dom
 import qualified "containers"   Data.Map as M
 import           "base"         Data.List (intersperse)
@@ -21,10 +24,11 @@ data LintStatus =
 
 type WWidget w = forall t m . (MonadWidget t m) => m (w t)
 
+defConfig :: Settings.LintSettings
+defConfig = Settings.defaultLintSettings
+
 lintString :: String -> LintStatus
 lintString str = do
-  let defConfig = Settings.defaultLintSettings
-
   let parsed = Util.parseFile defConfig "input" str
 
   case parsed of
@@ -33,6 +37,17 @@ lintString str = do
       xs -> Warnings $ map ($"input") xs
     Right (warnings, _) -> Warnings warnings
     Left errs -> SyntaxErrors errs
+
+prettyPrint :: String -> String
+prettyPrint lua =
+  let
+    parsed = P.parseGLuaFromString lua
+    ast = fst parsed
+    ppconf = Settings.lint2ppSetting defConfig
+    pretty = PP.prettyprintConf ppconf $ DarkRP.fixOldDarkRPSyntax ast
+  in
+    pretty
+
 
 prettyPrintMessages :: [LintMessage] -> [String]
 prettyPrintMessages = intersperse "\n" . map show . sortLintMessages
@@ -45,18 +60,26 @@ displayMessage (SyntaxErrors msgs) = prettyPrintMessages msgs
 
 main :: IO ()
 main = mainWidget $ do
+  prettyPrintButton <- btnPrettyPrint
+
   rec
       lintStatus <- mapDyn lintString $ _textArea_value t
       lintResults <- mapDyn displayMessage lintStatus
+      prettyPrinted <- mapDyn prettyPrint $ _textArea_value t
+
+      let prettyPrintOnClick = tag (current prettyPrinted) prettyPrintButton
 
       statusMessage lintStatus
       lintResultList lintResults
 
-      t <- txtLuaInput
-
-      return ()
+      t <- txtLuaInput prettyPrintOnClick
 
   return ()
+
+
+-- | Pretty print button
+btnPrettyPrint :: MonadWidget t m => m (Event t ())
+btnPrettyPrint = button "Pretty print"
 
 -- | Status message
 statusMessage :: (MonadWidget t m) => Dynamic t LintStatus -> m ()
@@ -76,9 +99,10 @@ lintResultList :: (MonadWidget t m) => Dynamic t [String] -> m ()
 lintResultList lintResults = void $ el "div" $ simpleList lintResults (el "p" . dynText)
 
 -- | Where you input your Lua
-txtLuaInput :: WWidget TextArea
-txtLuaInput =
+txtLuaInput :: (MonadWidget t m) => Event t String -> m (TextArea t)
+txtLuaInput setText =
   elAttr "div" (M.fromList [("style", "width: 100%; min-height: 500pt")]) $ textArea $
     def
     & textAreaConfig_initialValue .~ "-- Put your Lua here\n"
     & textAreaConfig_attributes .~ (constDyn $ "style" =: "width: 100%; min-height: 500pt")
+    & textAreaConfig_setValue .~ setText
