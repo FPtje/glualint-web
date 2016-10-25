900ec884716b4edb030da2b79e6058843ab1e78d
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-25 22:47:45 +0200

    Better layout of code

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,16 @@
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
 import qualified "glualint-lib" GLuaFixer.LintSettings as Settings
+import qualified "glualint-lib" GLuaFixer.AG.ASTLint as Lint
 import           "glualint-lib" GLuaFixer.LintMessage (LintMessage (..), sortLintMessages)
 import qualified "glualint-lib" GLuaFixer.Util as Util
-import qualified "glualint-lib" GLua.PSParser as P
-import qualified "glualint-lib" GLua.PSLexer as L
-import           "safe"         Safe (readMay)
-import qualified "reflex"       Reflex
-import qualified "reflex-dom"   Reflex.Dom as R
-import qualified                Data.Map as Map
-import                          Control.Applicative ((<*>), (<$>))
+import           "reflex-dom"   Reflex.Dom
+import qualified "containers"   Data.Map as M
 import           "base"         Data.List (intersperse)
+import           "base"         Control.Monad (void)
 
 
 data LintStatus =
@@ -21,6 +19,7 @@ data LintStatus =
   | SyntaxErrors [LintMessage] -- Syntax errors!
 
 
+type WWidget w = forall t m . (MonadWidget t m) => m (w t)
 
 lintString :: String -> LintStatus
 lintString str = do
@@ -29,7 +28,9 @@ lintString str = do
   let parsed = Util.parseFile defConfig "input" str
 
   case parsed of
-    Right ([], _) -> Good
+    Right ([], ast) -> case Lint.astWarnings defConfig ast of
+      [] -> Good
+      xs -> Warnings $ map ($"input") xs
     Right (warnings, _) -> Warnings warnings
     Left errs -> SyntaxErrors errs
 
@@ -37,21 +38,47 @@ prettyPrintMessages :: [LintMessage] -> [String]
 prettyPrintMessages = intersperse "\n" . map show . sortLintMessages
 
 displayMessage :: LintStatus -> [String]
-displayMessage Good = ["Your script is all good!"]
-displayMessage (Warnings msgs) = ["Your script will not throw syntax errors, but there are some warnings:", ""] ++ (prettyPrintMessages msgs)
-displayMessage (SyntaxErrors msgs) = ["Your script is broken! Check out these errors!", ""] ++ (prettyPrintMessages msgs)
+displayMessage Good = []
+displayMessage (Warnings msgs) = prettyPrintMessages msgs
+displayMessage (SyntaxErrors msgs) = prettyPrintMessages msgs
 
 
 main :: IO ()
-main = R.mainWidget $ do
+main = mainWidget $ do
   rec
-      lintResults <- R.mapDyn (displayMessage . lintString) $ R._textArea_value t
+      lintStatus <- mapDyn lintString $ _textArea_value t
+      lintResults <- mapDyn displayMessage lintStatus
 
-      R.el "div" $ R.simpleList lintResults (R.el "div" . R.dynText)
-      -- R.el "div" $ R.dynText lintResult
-      t <- R.el "div" $ R.textArea $ R.def
-        R.& R.textAreaConfig_initialValue R..~ "-- Put your Lua here"
+      statusMessage lintStatus
+      lintResultList lintResults
+
+      t <- txtLuaInput
 
       return ()
 
   return ()
+
+-- | Status message
+statusMessage :: (MonadWidget t m) => Dynamic t LintStatus -> m ()
+statusMessage lintStatus =
+  let
+    status :: LintStatus -> String
+    status Good = "Your script is all good!"
+    status (Warnings _) = "Your script will not throw syntax errors, but there are some warnings!"
+    status (SyntaxErrors _) = "Your script is broken! Check out these errors!"
+  in do
+    txt <- mapDyn status lintStatus
+
+    el "p" $ dynText txt
+
+-- | Displays a list paragraphs with warnings and/or errors
+lintResultList :: (MonadWidget t m) => Dynamic t [String] -> m ()
+lintResultList lintResults = void $ el "div" $ simpleList lintResults (el "p" . dynText)
+
+-- | Where you input your Lua
+txtLuaInput :: WWidget TextArea
+txtLuaInput =
+  elAttr "div" (M.fromList [("style", "width: 100%; min-height: 500pt")]) $ textArea $
+    def
+    & textAreaConfig_initialValue .~ "-- Put your Lua here\n"
+    & textAreaConfig_attributes .~ (constDyn $ "style" =: "width: 100%; min-height: 500pt")
