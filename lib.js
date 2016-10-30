dbee4fa3a2c599d1fff32873f49b2ffac88a7516
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-30 13:30:07 +0100

    Some styling

--- lib.js
+++ lib.js
@@ -2397,7 +2397,7 @@ var editor;
 var editorElement;
 function createCodeMirror(initValue, mode, theme) {
   editor = CodeMirror(function(elt) { editorElement = elt; }, {value: initValue, mode: mode, lineNumbers: true, theme: theme, autofocus: true});
-  setTimeout(function() { document.body.appendChild(editorElement); editor.refresh() });
+  setTimeout(function() { document.getElementById("content").appendChild(editorElement); editor.refresh() });
 }
 function getEditor() { return editor; };
 function createOnEvent(tp, f) {editor.on(tp, function() { f(); })};
