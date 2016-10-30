5224139b6d8f29f1abae9586b227401d1febb8c5
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-30 19:40:25 +0100

    Threading update

--- lib.js
+++ lib.js
@@ -2397,7 +2397,12 @@ var editor;
 var editorElement;
 function createCodeMirror(initValue, mode, theme) {
   editor = CodeMirror(function(elt) { editorElement = elt; }, {value: initValue, mode: mode, lineNumbers: true, theme: theme, autofocus: true});
-  setTimeout(function() { document.getElementById("content").appendChild(editorElement); editor.refresh() });
+  editor.setCursor(1, 0)
+  setTimeout(function() {
+    document.getElementById("content").appendChild(editorElement);
+    editor.refresh();
+    editor.focus();
+  });
 }
 function getEditor() { return editor; };
 function createOnEvent(tp, f) {editor.on(tp, function() { f(); })};
