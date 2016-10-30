30337602a7e4cc4b5fd4ba37106633698c5d1cca
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-30 15:19:19 +0100

    Cursor at end on startup

--- jsbits/editor.js
+++ jsbits/editor.js
@@ -4,6 +4,7 @@ var editor;
 var editorElement;
 function createCodeMirror(initValue, mode, theme) {
   editor = CodeMirror(function(elt) { editorElement = elt; }, {value: initValue, mode: mode, lineNumbers: true, theme: theme, autofocus: true});
+  editor.setCursor(1, 0)
   setTimeout(function() { document.getElementById("content").appendChild(editorElement); editor.refresh() });
 }
 
